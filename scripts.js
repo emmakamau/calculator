c6c09e5d57493255849c425276d2a13158c6d742
EmmaK <emmaculatewkamau@gmail.com>
2022-03-04 10:22:04 +0300

    Add all functions

--- scripts.js
+++ scripts.js
@@ -1,110 +1,37 @@
-var add = (numOne,numTwo) => numOne+numTwo;
-var subtract = (numOne,numTwo) => numOne-numTwo;
-var multiply = (numOne,numTwo) => numOne*numTwo;
-var divide = (numOne,numTwo) => numOne/numTwo;
-var reminder = (numOne,numTwo) => numOne%numTwo;
-
-//var numOne=parseInt(prompt('Enter a number:'));
-//var numTwo=parseInt(prompt('Enter another number:'))
-
-var result = reminder(numOne,numTwo);
-
-alert(result)
-
-
-/*
-//BMI calculator
-var bmi = (weight, height) => {
-    var height = parseFloat(prompt("What is your height?"));
-    var weight = parseInt(prompt("What is your weight?"));
-    let myHeight = height*height;
-    let myBmi = weight/myHeight;
-    if (myBmi > 25) {
-      alert('Opps! your BMI is ' + myBmi + ' , consider loosing some weight')
-    } else {
-      alert('Your BMI is' + myBmi + ', you are in perfect health')
-    }
-  }
-  
-  bmi()
-
-
-  //Adds two numbers and returns an alert
-  var myFunction = (name) =>{
-	var name=prompt('What is your name?');
-    }
-
-    var addNum = (num1,num2)=>{
-        var num1 = parseInt(prompt("Enter a number:"));
-        var num2 = parseInt(prompt("Enter a second number:"));
-        var mySum = num1+num2;
-        alert('My name is '+name+' and the sum of my numbers is '+mySum)
-    };
-
-    myFunction()
-    addNum()
-
-//Temp converter
-
-var temperature = (fah)=>{
-	var fah = parseFloat(prompt("Enter temperature in Fahrenheit:"))
-	var celcius = (fah-32)/1.8;
-  
-  alert(fah+'Fahrenheit is equal to '+celcius+'degrees celcius');
+function inputOne(){
+  let numOne = document.getElementById("inputOne").value
+  return parseInt(numOne);
 }
 
-temperature()
-
-
-
-//Even Number calculator
-
-var evenNumber = (num)  => {
-	var num = parseInt(prompt('Enter a whole number:'))
-	if (num%2 == 0){
-  alert(num+' is an even number')
-  }else{
-  alert(num+' is an odd number')
-  }
+function inputTwo(){
+  let numTwo = document.getElementById("inputTwo").value
+  return parseInt(numTwo);
 }
 
-evenNumber()
-
-
-//Grade app
-
-var myGrade = (grade)=>{
-	grade = parseInt(prompt('Fill in marks:'));
-  if (grade >= 80){
-  	result = "A";
-  }else if (grade >=60 && grade<=79 ){
-  	result = "B"
-  }else if (grade >=40 && grade<= 59){
-  	result = "C";
-  }else if (grade >= 20 && grade<=39){
-  	result = "D";
-  }else{
-  	result ="E";
-  }
-  alert ("Grade: "+result)
+function add(){
+  let numOne = inputOne()
+  let numTwo = inputTwo()
+  let sum = numOne + numTwo;
+  document.getElementById('output').innerHTML = `The sum of ${numOne} and ${numTwo} is ${sum}.`
 }
 
-myGrade()
-
-
-//var leapYearRange = (startYear,endYear) => {
-	var startYear = parseInt(prompt("From year"));
-  var endYear = parseInt(prompt("End year"));
-  var count = 0;
-  
-  for (let i = startYear; i <= endYear; i++){
-  	if (i%4 === 0){
-    	count = count+1;
-    }
-  }
-  alert('There are '+count+'leap years between '+startYear+' and '+endYear);
+function subtract(){
+  let numOne = inputOne()
+  let numTwo = inputTwo()
+  let difference = numOne - numTwo;
+  document.getElementById('output').innerHTML = `The difference between ${numOne} and ${numTwo} is ${difference}.`
 }
 
-leapYearRange()
+function multiply(){
+  let numOne = inputOne()
+  let numTwo = inputTwo()
+  let product = numOne * numTwo;
+  document.getElementById('output').innerHTML = `The product of ${numOne} and ${numTwo} is ${product}.`
+}
 
-*/
+function divide(){
+  let numOne = inputOne()
+  let numTwo = inputTwo()
+  let division = numOne / numTwo;
+  document.getElementById('output').innerHTML = `The division of ${numOne} and ${numTwo} is ${division}.`
+}
